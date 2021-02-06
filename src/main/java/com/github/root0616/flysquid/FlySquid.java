eba5f37594942e6f191cbd82b6e2d90d9ee5d67a
RooT0616 <matumokencave@gmail.com>
2021-02-06 17:44:55 +0900

    2021/02/06

--- src/main/java/com/github/root0616/flysquid/FlySquid.java
+++ src/main/java/com/github/root0616/flysquid/FlySquid.java
@@ -2,16 +2,15 @@ package com.github.root0616.flysquid;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class FlySquid extends JavaPlugin {
+public class FlySquid extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+	@Override
+	public void onEnable() {
+		// 他のクラスから登録する場合
+	}
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+	@Override
+	public void onDisable() {
+		// 特定のイベントだけを解除する場合
+	}
 }
