5ae1c4b261e8fad652d632027c5e6ab5c979c9c1
RooT0616 <matumokencave@gmail.com>
2021-02-07 15:31:25 +0900

    エンティティに乗れるように修正

--- src/main/java/com/github/root0616/flysquid/FlySquid.java
+++ src/main/java/com/github/root0616/flysquid/FlySquid.java
@@ -1,16 +1,29 @@
 package com.github.root0616.flysquid;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FlySquid extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+    // Plugin startup logic
+    getLogger().info("プラグインが開始しました");
 		// 他のクラスから登録する場合
+		// コンストラクタで登録する場合
+		new RideAnimal(this);
 	}
 
 	@Override
 	public void onDisable() {
 		// 特定のイベントだけを解除する場合
+		// 全てのイベントを解除する場合
+		HandlerList.unregisterAll(this);
+    // Plugin shutdown logic
+    getLogger().info("プラグインが停止しました");
 	}
 }
